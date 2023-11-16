e14a97132d45d405dd5ab393881b938e32a73d5a
rsoups <rethun@my.monroeccc.edu>
2023-11-16 02:35:35 -0500

    LogIn to GamePage
    made connection between our first two pages. will add dummy data for the games next

---
+++ ttrpgManager/app/src/main/java/com/example/ttrpgmanager/GamesPage.java
@@ -0,0 +1,14 @@
+package com.example.ttrpgmanager;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class GamesPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_games_page);
+    }
+}
--- ttrpgManager/app/src/main/java/com/example/ttrpgmanager/MainActivity.java
+++ ttrpgManager/app/src/main/java/com/example/ttrpgmanager/MainActivity.java
@@ -2,13 +2,37 @@ package com.example.ttrpgmanager;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity
+{
+    Button btn_j_logIn;
+    Intent gamePageIntent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //LogIn Button -> GamesPage
+        gamePageIntent = new Intent(MainActivity.this, GamesPage.class);
+
+        btn_j_logIn = findViewById(R.id.btn_v_logIn);
+
+        logInEventHandler();
+    }
+
+    public void logInEventHandler()
+    {
+        btn_j_logIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(gamePageIntent);
+            }
+        });
     }
 }
